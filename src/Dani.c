2c93abb34a319be16bdbe4c9b65b2ac78d9078a1
Israel Torres <itorres1490@gmail.com>
2015-05-15 19:44:46 -0700

    Removed redundant function call by adding on start flag. Currently broken because of animation.

--- src/Dani.c
+++ src/Dani.c
@@ -17,7 +17,7 @@ static void init() {
   });
 
   window_stack_push(s_main_window, true);
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
 }
 
 static void deinit() {
@@ -25,38 +25,35 @@ static void deinit() {
 }
 
 static void main_window_load(Window *window) {
-  fill_hour_frames();
-  set_postions();
-  text_layer_set_font(text_layers[0], fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
-  text_layer_set_text(text_layers[0], "00");
-  text_layer_set_background_color(text_layers[0], GColorClear);
-  text_layer_set_text_color(text_layers[0], GColorWhite);
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
 
-  text_layer_set_font(text_layers[1], fonts_get_system_font(FONT_KEY_BITHAM_34_MEDIUM_NUMBERS));
-  text_layer_set_text(text_layers[1], "12");
-  text_layer_set_background_color(text_layers[1], GColorClear);
-  text_layer_set_text_color(text_layers[1], GColorWhite);
+  startup = true;
+
+  fill_hour_frames();
+  update_time(tick_time);
+
+  free(tick_time);
+
+  updateTimeLayers();
 
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layers[0]));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layers[1]));
-
-  update_time();
 }
 
-static void main_window_unload(Window *window) {
+static void main_window_unload() {
   text_layer_destroy(text_layers[0]);
   text_layer_destroy(text_layers[1]);
   property_animation_destroy(s_property_animation);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_time();
+  update_time(tick_time);
 }
 
-static void update_time() {
-  time_t temp = time(NULL);
-  struct tm *tick_time = localtime(&temp);
-
+static void update_time(struct tm *tick_time) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Heap Used: %d", heap_bytes_used());
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Heap Free: %d", heap_bytes_free());
   static char hours[] = "00";
   static char mins[] = "00";
 
@@ -65,6 +62,7 @@ static void update_time() {
 
   trigger_custom_animation(tick_time);
   text_layer_set_text(text_layers[0], mins);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "1");
 
   static char single_hour[] = "";
   single_hour[0] = (char) hours[1];
@@ -80,49 +78,11 @@ static void update_time() {
   } else {
     text_layer_set_text(text_layers[1], hours);
   }
-}
-
-static void set_postions() {
-  time_t temp = time(NULL);
-  struct tm *tick_time = localtime(&temp);
-
-  int x_pos, y_pos, mins, hours;
-  mins = tick_time->tm_min;
-  hours = tick_time->tm_hour;
-
-  if(hours > 12) {
-    hours -= 12;
-  }
-
-  if((mins >= 0) && (mins <= 5)) {
-    x_pos = (mins * 6) + 60;
-    text_layers[0] = text_layer_create(GRect(x_pos, 0, TM_SIZE, TM_SIZE));
-  }
-
-  if((mins >= 6) && (mins <= 24)) {
-    y_pos = (mins - 6) * 8;
-    text_layers[0] = text_layer_create(GRect(MIN_LEFT, y_pos, TM_SIZE, TM_SIZE));
-  }
-
-  if((mins >= 25) && (mins <= 35)) {
-    x_pos = (35 - mins) * 13;
-    text_layers[0] = text_layer_create(GRect(x_pos, MIN_BOTTOM, TM_SIZE, TM_SIZE));
-  }
-
-  if((mins >= 36) && (mins <= 54)) {
-    y_pos = (54 - mins) * 8;
-    text_layers[0] = text_layer_create(GRect(0, y_pos, TM_SIZE, TM_SIZE));
-  }
-
-  if((mins >= 55) && (mins <= 59)) {
-    x_pos = (mins - 55) * 6;
-    text_layers[0] = text_layer_create(GRect(x_pos, 0, TM_SIZE, TM_SIZE));
-  }
-
-  text_layers[1] = text_layer_create(hour_pos[hours]);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "2");
 }
 
 static void trigger_custom_animation(struct tm *tick_time) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "3");
   GRect from_frame_mins = layer_get_frame((Layer *)text_layers[0]);
   GRect to_frame_mins;
 
@@ -141,33 +101,72 @@ static void trigger_custom_animation(struct tm *tick_time) {
     mins += 60;
   }
 
+  if(startup) {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "4");
+    //RIGHT
+    if((mins >= 6) && (mins <= 24)) {
+      y_pos = ((mins - 6) * 7) + 13;
+      text_layers[0] = text_layer_create(GRect(MIN_LEFT, y_pos, TM_SIZE, TM_SIZE));
+    }
+
+    //LEFT
+    if((mins >= 36) && (mins <= 54)) {
+      y_pos = ((54 - mins) * 9) + 13;
+      text_layers[0] = text_layer_create(GRect(50, y_pos, TM_SIZE, TM_SIZE));
+    }
+
+    //BOTTOM
+    if((mins >= 25) && (mins <= 35)) {
+      x_pos = ((35 - mins) * 8) + 50;
+      text_layers[0] = text_layer_create(GRect(x_pos, MIN_BOTTOM, TM_SIZE, TM_SIZE));
+    }
+
+    //TOP
+    if((mins >= 55) && (mins <= 65)) {
+      x_pos = ((mins - 55) * 8) + 50;
+      text_layers[0] = text_layer_create(GRect(x_pos, 0, TM_SIZE, TM_SIZE));
+    }
+
+    text_layers[1] = text_layer_create(hour_pos[hours]);
+
+    startup = false;
+  }
+
+  //RIGHT
   if((mins >= 6) && (mins <= 24)) {
     y_pos = ((mins - 6) * 7) + 13;
     to_frame_mins = GRect(MIN_LEFT, y_pos, TM_SIZE, TM_SIZE);
   }
 
+  //LEFT
   if((mins >= 36) && (mins <= 54)) {
     y_pos = ((54 - mins) * 7) + 13;
-    to_frame_mins = GRect(0, y_pos, TM_SIZE, TM_SIZE);
+    to_frame_mins = GRect(50, y_pos, TM_SIZE, TM_SIZE);
   }
 
+  //BOTTOM
   if((mins >= 25) && (mins <= 35)) {
-    x_pos = ((35 - mins) * 13);
+    x_pos = ((35 - mins) * 8) + 50;
     to_frame_mins = GRect(x_pos, MIN_BOTTOM, TM_SIZE, TM_SIZE);
   }
 
+  //TOP
   if((mins >= 55) && (mins <= 65)) {
-    x_pos = ((mins - 55) * 13) + 2;
+    x_pos = ((mins - 55) * 8) + 50;
     to_frame_mins = GRect(x_pos, 0, TM_SIZE, TM_SIZE);
   }
 
   to_frame_hours = hour_pos[hours];
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "5");
 
   s_property_animation = property_animation_create_layer_frame((Layer *)text_layers[1], &from_frame_hours, &to_frame_hours);
   animation_schedule((Animation*) s_property_animation);
 
   s_property_animation = property_animation_create_layer_frame((Layer *)text_layers[0], &from_frame_mins, &to_frame_mins);
   animation_schedule((Animation*) s_property_animation);
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "6");
 }
 
 static void fill_hour_frames() {
@@ -177,11 +176,23 @@ static void fill_hour_frames() {
   hour_pos[3] = GRect(INNER_RIGHT - 2, INNER_BOTTOM / 2, TH_SIZE_X, TH_SIZE_Y);
   hour_pos[4] = GRect(INNER_RIGHT - 4, INNER_BOTTOM - 30, TH_SIZE_X, TH_SIZE_Y);
   hour_pos[5] = GRect(INNER_RIGHT - 2, INNER_BOTTOM, TH_SIZE_X, TH_SIZE_Y);
-  hour_pos[6] = GRect(INNER_LEFT + 45, INNER_BOTTOM, TH_SIZE_X, TH_SIZE_Y);
-  hour_pos[7] = GRect(INNER_LEFT - 1, INNER_BOTTOM, TH_SIZE_X, TH_SIZE_Y);
-  hour_pos[8] = GRect(INNER_LEFT - 1, INNER_BOTTOM - 30, TH_SIZE_X, TH_SIZE_Y);
+  hour_pos[6] = GRect(INNER_LEFT + 58, INNER_BOTTOM, TH_SIZE_X, TH_SIZE_Y);
+  hour_pos[7] = GRect(INNER_LEFT, INNER_BOTTOM, TH_SIZE_X, TH_SIZE_Y);
+  hour_pos[8] = GRect(INNER_LEFT, INNER_BOTTOM - 30, TH_SIZE_X, TH_SIZE_Y);
   hour_pos[9] = GRect(INNER_LEFT - 1, INNER_BOTTOM / 2, TH_SIZE_X, TH_SIZE_Y);
-  hour_pos[10] = GRect(INNER_LEFT, (INNER_BOTTOM / 4) + 2, TH_SIZE_X, TH_SIZE_Y);
-  hour_pos[11] = GRect(INNER_LEFT, INNER_TOP, TH_SIZE_X, TH_SIZE_Y);
-  hour_pos[12] = GRect(INNER_LEFT + 43, INNER_TOP, TH_SIZE_X, TH_SIZE_Y);
+  hour_pos[10] = GRect(INNER_LEFT - 14, (INNER_BOTTOM / 4) + 2, TH_SIZE_X, TH_SIZE_Y);
+  hour_pos[11] = GRect(INNER_LEFT - 2, INNER_TOP, TH_SIZE_X, TH_SIZE_Y);
+  hour_pos[12] = GRect(INNER_LEFT + 53, INNER_TOP, TH_SIZE_X, TH_SIZE_Y);
+}
+
+static void updateTimeLayers() {
+  text_layer_set_font(text_layers[0], fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
+  text_layer_set_text(text_layers[0], "");
+  text_layer_set_background_color(text_layers[0], GColorClear);
+  text_layer_set_text_color(text_layers[0], GColorWhite);
+
+  text_layer_set_font(text_layers[1], fonts_get_system_font(FONT_KEY_BITHAM_34_MEDIUM_NUMBERS));
+  text_layer_set_text(text_layers[1], "");
+  text_layer_set_background_color(text_layers[1], GColorClear);
+  text_layer_set_text_color(text_layers[1], GColorWhite);
 }
--- src/Dani.h
+++ src/Dani.h
@@ -1,24 +1,25 @@
 #define TM_SIZE 15
-#define MIN_LEFT 132
+#define MIN_LEFT 130
 #define MIN_BOTTOM 153
 
 #define TH_SIZE_X 35
 #define TH_SIZE_Y 35
-#define INNER_LEFT 15
+#define INNER_LEFT 27
 #define INNER_RIGHT 114
-#define INNER_TOP 5
-#define INNER_BOTTOM 120
+#define INNER_TOP 9
+#define INNER_BOTTOM 119
 
 static Window *s_main_window;
 static TextLayer *text_layers[2];
 static GRect hour_pos[13];
 static PropertyAnimation *s_property_animation;
+static bool startup;
 
-static void update_time();
+static void update_time(struct tm *tick_time);
 static void main_window_load(Window *window);
-static void main_window_unload(Window *window);
-static void set_postions();
+static void main_window_unload();
 static void fill_hour_frames();
+static void updateTimeLayers();
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed);
 static void trigger_custom_animation(struct tm *tick_time);
 static void init();
